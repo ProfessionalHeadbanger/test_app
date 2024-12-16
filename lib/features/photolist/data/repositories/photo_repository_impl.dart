e33b78727bc6a5918d4e4e1d3ba7510faeb4a0ca
ProfessionalHeadbanger <nice.sevkabr@mail.ru>
2024-12-16 23:17:42 +0300

    fix(SI-11735)-API-layer: fixed converting shadowColor from String to Color, removed Either from dartz package

--- lib/features/photolist/data/repositories/photo_repository_impl.dart
+++ lib/features/photolist/data/repositories/photo_repository_impl.dart
@@ -1,12 +1,14 @@
 import 'package:test_app/core/params/params.dart';
 import 'package:test_app/features/photolist/data/datasources/photos_remote_data_source.dart';
 import 'package:test_app/features/photolist/data/models/photo_model.dart';
+import 'package:test_app/features/photolist/domain/repositories/photo_repository.dart';
 
-class PhotoRepositoryImpl {
+class PhotoRepositoryImpl extends PhotoRepository {
   final PhotosRemoteDataSource photosRemoteDataSource;
 
   PhotoRepositoryImpl(this.photosRemoteDataSource);
 
+  @override
   Future<List<PhotoModel>> getPageOfPhotos(
       {required PhotoPageParams photoPageParams}) async {
     final photoList = await photosRemoteDataSource.getPageOfPhotos(
